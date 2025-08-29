bd930f3bca22e46bba318385f53f9d2df2e0ccae
sadianusratmunny51 <sadiamunny51@gmail.com>
2025-08-29 19:22:29 +0600

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hello');
+const express = require('express');
+// const taskRoutes = require('./routes/taskRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+// app.use('/tasks', taskRoutes);
+
+app.listen(PORT, () => {
+  console.log(` Server running on http://localhost:${PORT}`);
+});
